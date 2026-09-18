5015f5a75d722535403f4eb1ac83e9c1370c7436
agrodeo <faustosicilia123@gmail.com>
2026-09-18 20:44:46 -0300

    Expose profile-aware dry runs in admin

--- src/routes/_authenticated/admin.tsx
+++ src/routes/_authenticated/admin.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
 import { AdapterTestConsole } from "@/components/adapter-test-console";
+import { AdminDryRunConsole } from "@/components/admin-dry-run-console";
 import { GreenhouseDiscoveryConsole } from "@/components/greenhouse-discovery-console";
 import { PageShell, SiteHeader } from "@/components/aplica";
 import { getAdminDashboard } from "@/lib/admin.functions";
@@ -84,6 +85,7 @@ function AdminPage() {
             </div>
 
             <GreenhouseDiscoveryConsole />
+            <AdminDryRunConsole />
             <AdapterTestConsole />
           </>
         )}
